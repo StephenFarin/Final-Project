8a6dfbf6f83aaf7146c759c927c8553e37ab35e5
Divine-101 <103386388+Divine-101@users.noreply.github.com>
2022-04-26 13:20:01 -0500

    Update game.py

--- game.py
+++ game.py
@@ -60,6 +60,21 @@ def runGame():
                   {'x': startx - 2, 'y': starty}]
     direction = RIGHT
     apple = getRandomLocation()
+    while True: # main game loop
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
+                     direction = LEFT
+                elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
+                    direction = RIGHT
+                elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
+                    direction = UP
+                elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
+                    direction = DOWN
+                elif event.key == K_ESCAPE:
+                    terminate()
     
 def getRandomLocation():
     pass
