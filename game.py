8f0f9e1aaa165a0e7a38dff6f087b1c0641b2cfe
Divine-101 <103386388+Divine-101@users.noreply.github.com>
2022-04-26 13:02:04 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,57 @@
-import pygame as pg
-import time as t
+import pygame, sys
+from pygame.locals import *
 
-#snake colors
-black = pg.Color(0,0,0)
-white = pg.Color
-blue = pg.Color
+pygame.init()
+FPS = 15
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+CELLSIZE = 20
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
+Aqua = (0, 255, 255)
+Black = (0, 0, 0)
+Blue = (0, 0, 255)
+Fuchsia = (255, 0, 255)
+Gray = (128, 128, 128)
+Green = (0, 128, 0)
+Lime = (0, 255, 0)
+Maroon = (128, 0, 0)
+NavyBlue = (0, 0, 128)
+Olive = (128, 128, 0)
+Purple = (128, 0, 128)
+Red = (255, 0, 0)
+Silver = (192, 192, 192)
+Teal = (0, 128, 128)
+White = (255, 255, 255)
+Yellow = (255, 255, 0)
+BGCOLOR = Black
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+HEAD = 0
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    pygame.display.set_caption('Snek')
+    
+    mainmenu()
+    while True:
+        runGame()
+
+def mainmenu():
+    pass
+
+def runGame():
+    pass
+
+def gameOver():
+    pass
+  
+def options():
+    pass
